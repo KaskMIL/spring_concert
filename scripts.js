6a70f793ec7f815ec70da5fe2219748ea3787e2f
toMilanesi <tomi.milanesi@gmail.com>
2022-05-17 18:15:17 -0300

    Add desktop version  of header

--- scripts.js
+++ scripts.js
@@ -2,18 +2,40 @@ const openMenuBtn = document.getElementById('menu-open-btn');
 const menuNavHamburger = document.getElementById('menu-nav-ham');
 const menuNavOpen = document.getElementById('menu-nav-open');
 const closeMenuBtn = document.getElementById('menu-close-btn');
+const introBtn = document.getElementById('intro-btn');
+const spacesBtn = document.getElementById('spaces-btn');
+const bandsBtn = document.getElementById('bands-btn');
+const introSection = document.getElementById('intro');
+const sapcesSection = document.getElementById('spaces');
+const bandsSection = document.getElementById('bands');
 const body = document.body;
+
+function closeMenu() {
+  menuNavHamburger.classList.remove('mobile-menu-off');
+  menuNavOpen.classList.add('mobile-menu-off');
+  menuNavOpen.classList.remove('mobile-menu-on');
+  body.classList.remove('disable-scroll');
+};
 
 openMenuBtn.addEventListener('click', () => {
   menuNavHamburger.classList.add('mobile-menu-off');
   menuNavOpen.classList.remove('mobile-menu-off');
   menuNavOpen.classList.add('mobile-menu-on');
   body.classList.add('disable-scroll');
-})
+});
 
 closeMenuBtn.addEventListener('click', () => {
-  menuNavHamburger.classList.remove('mobile-menu-off');
-  menuNavOpen.classList.add('mobile-menu-off');
-  menuNavOpen.classList.remove('mobile-menu-on');
-  body.classList.remove('disable-scroll');
-})
+  closeMenu();
+});
+
+introBtn.addEventListener('click', () => {
+  closeMenu();
+});
+
+spacesBtn.addEventListener('click', () => {
+  closeMenu();
+});
+
+bandsBtn.addEventListener('click', () => {
+  closeMenu();
+});
